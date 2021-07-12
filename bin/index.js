9c9e941eedd47e3b450d3bcae61d94b9aef3ba2d
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-07-12 12:37:30 -0400

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,6 @@ const path = require('path');
 let config_file;
 if (argv.config) config_file = argv.config;
 const configPath = path.resolve(process.cwd(), config_file || 'config.js');
-const config = require(config_file);
+const config = require(configPath);
 
 pullContent.runQueries(config);

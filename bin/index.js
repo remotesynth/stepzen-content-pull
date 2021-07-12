ef7099a9644b2d419a66747e2a69b6c7c4d72b46
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-07-12 12:37:13 -0400

    changing how config is loaded

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const pullContent = require('../lib/pullContent');
 const argv = require('minimist')(process.argv.slice(2));
+const path = require('path');
 
-let config_file = '../config.js';
+let config_file;
 if (argv.config) config_file = argv.config;
+const configPath = path.resolve(process.cwd(), config_file || 'config.js');
 const config = require(config_file);
 
 pullContent.runQueries(config);
